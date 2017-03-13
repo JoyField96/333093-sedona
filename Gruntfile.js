cff9915ddf026a3652d42b84093a5390f1603be6
JoyField96 <mor.96@mail.ru>
2017-03-13 09:09:22 +0300

    поправил все

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.initConfig({
+    sass: {
+      style: {
+        files: {
+          "css/style.css": "sass/style.scss"
+        }
+      }
+    }
+  });
+};
